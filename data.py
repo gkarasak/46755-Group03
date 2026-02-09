0755f86802f221e093b3b3163ba1b5aadb9b3b67
inesmartizq <s243349@dtu.dk>
2026-02-09 16:14:12 +0100

    created dictionaries with the data

---
+++ data.py
@@ -0,0 +1,204 @@
+# Generator Technical Data Dictionary
+
+generators = {
+    'G1': {
+        'Unit': 1,
+        'Node': 1,
+        'Pmax_MW': 152,
+        'Pmin_MW': 0
+    },
+    'G2': {
+        'Unit': 2,
+        'Node': 2,
+        'Pmax_MW': 152,
+        'Pmin_MW': 0
+    },
+    'G3': {
+        'Unit': 3,
+        'Node': 7,
+        'Pmax_MW': 350,
+        'Pmin_MW': 0
+    },
+    'G4': {
+        'Unit': 4,
+        'Node': 13,
+        'Pmax_MW': 591,
+        'Pmin_MW': 0
+    },
+    'G5': {
+        'Unit': 5,
+        'Node': 15,
+        'Pmax_MW': 60,
+        'Pmin_MW': 0
+    },
+    'G6': {
+        'Unit': 6,
+        'Node': 15,
+        'Pmax_MW': 155,
+        'Pmin_MW': 0
+    },
+    'G7': {
+        'Unit': 7,
+        'Node': 16,
+        'Pmax_MW': 155,
+        'Pmin_MW': 0
+    },
+    'G8': {
+        'Unit': 8,
+        'Node': 18,
+        'Pmax_MW': 400,
+        'Pmin_MW': 0
+    },
+    'G9': {
+        'Unit': 9,
+        'Node': 21,
+        'Pmax_MW': 400,
+        'Pmin_MW': 0
+    },
+    'G10': {
+        'Unit': 10,
+        'Node': 22,
+        'Pmax_MW': 300,
+        'Pmin_MW': 0
+    },
+    'G11': {
+        'Unit': 11,
+        'Node': 23,
+        'Pmax_MW': 310,
+        'Pmin_MW': 0
+    },
+    'G12': {
+        'Unit': 12,
+        'Node': 23,
+        'Pmax_MW': 350,
+        'Pmin_MW': 0
+    }
+}
+
+load_profile = {
+    1: 1775.835,
+    2: 1669.815,
+    3: 1590.3,
+    4: 1563.795,
+    5: 1563.795,
+    6: 1590.3,
+    7: 1961.37,
+    8: 2279.43,
+    9: 2517.975,
+    10: 2544.48,
+    11: 2544.48,
+    12: 2517.975,
+    13: 2517.975,
+    14: 2517.975,
+    15: 2464.965,
+    16: 2464.965,
+    17: 2623.995,
+    18: 2650.5,
+    19: 2650.5,
+    20: 2544.48,
+    21: 2411.955,
+    22: 2199.915,
+    23: 1934.865,
+    24: 1669.815
+}
+
+
+
+load_distribution = {
+    'Load1': {
+        'Node': 1,
+        'Percent_of_system_load': 3.8
+    },
+    'Load2': {
+        'Node': 2,
+        'Percent_of_system_load': 3.4
+    },
+    'Load3': {
+        'Node': 3,
+        'Percent_of_system_load': 6.3
+    },
+    'Load4': {
+        'Node': 4,
+        'Percent_of_system_load': 2.6
+    },
+    'Load5': {
+        'Node': 5,
+        'Percent_of_system_load': 2.5
+    },
+    'Load6': {
+        'Node': 6,
+        'Percent_of_system_load': 4.8
+    },
+    'Load7': {
+        'Node': 7,
+        'Percent_of_system_load': 4.4
+    },
+    'Load8': {
+        'Node': 8,
+        'Percent_of_system_load': 6
+    },
+    'Load9': {
+        'Node': 9,
+        'Percent_of_system_load': 6.1
+    },
+    'Load10': {
+        'Node': 10,
+        'Percent_of_system_load': 6.8
+    },
+    'Load11': {
+        'Node': 13,
+        'Percent_of_system_load': 9.3
+    },
+    'Load12': {
+        'Node': 14,
+        'Percent_of_system_load': 6.8
+    },
+    'Load13': {
+        'Node': 15,
+        'Percent_of_system_load': 11.1
+    },
+    'Load14': {
+        'Node': 16,
+        'Percent_of_system_load': 3.5
+    },
+    'Load15': {
+        'Node': 18,
+        'Percent_of_system_load': 11.7
+    },
+    'Load16': {
+        'Node': 19,
+        'Percent_of_system_load': 6.4
+    },
+    'Load17': {
+        'Node': 20,
+        'Percent_of_system_load': 4.5
+    }
+}
+
+
+load_profile = {
+    1: 1775.835,
+    2: 1669.815,
+    3: 1590.3,
+    4: 1563.795,
+    5: 1563.795,
+    6: 1590.3,
+    7: 1961.37,
+    8: 2279.43,
+    9: 2517.975,
+    10: 2544.48,
+    11: 2544.48,
+    12: 2517.975,
+    13: 2517.975,
+    14: 2517.975,
+    15: 2464.965,
+    16: 2464.965,
+    17: 2623.995,
+    18: 2650.5,
+    19: 2650.5,
+    20: 2544.48,
+    21: 2411.955,
+    22: 2199.915,
+    23: 1934.865,
+    24: 1669.815
+}
